ed024b5853d2f8c7240d64537e74296489020c03
rares-razvan.boldea <rares-razvan.boldea@student.upt.ro>
2026-04-13 11:33:41 +0300

    Finished SRT r4 register

--- test_benches/srtr4_reg_tb.v
+++ test_benches/srtr4_reg_tb.v
@@ -5,7 +5,7 @@ reg load,c_down,rshift,lshift1,lshift2;
 reg left_in;
 reg [1:0] right_in;
 reg [7:0] d;
-wire q;
+wire [7:0] q;
 
 srtr4_reg #(.size(8)) test
 (
@@ -21,5 +21,39 @@ srtr4_reg #(.size(8)) test
 	.d(d),
 	.q(q)
 );
+
+initial begin
+	{clk,rst_b,load,c_down,rshift,lshift1,lshift2}=7'b0100000;
+	{left_in,right_in}=3'b000;
+	d=6'b011001;
+	forever #50 clk=~clk;
+end
+
+initial begin
+	repeat(2) #10 rst_b=~rst_b;
+	load=1;
+	right_in=2'b10;
+	@(posedge clk) #10 load=0;
+	lshift1=1;
+	repeat(2) @(posedge clk);
+	lshift1=0;
+	@(posedge clk);
+	lshift2=1;
+	right_in=2'b00;
+	repeat(3) @(posedge clk);
+	lshift2=0;
+	@(posedge clk);
+	rshift=1;
+	repeat(2) @(posedge clk);
+	rshift=0;
+	@(posedge clk);
+	c_down=1;
+	@(posedge clk);
+	c_down=0;
+	@(posedge clk);
+	rshift=1;
+	
+	
+end
 
 endmodule
