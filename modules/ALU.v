62c8a969650db019873e374315cb89a523bdcfa2
Teregovan David <davidteregovan09@gmail.com>
2026-04-21 17:04:51 +0300

    added divider to ALU

--- modules/ALU.v
+++ modules/ALU.v
@@ -1,24 +1,24 @@
 module ALU
 (
-	input clk,rst_b,
-	input ADD,SUB,MULT,
-	input [7:0] x,y,
+	input clk, rst_b,
+	input ADD, SUB, MULT, DIV,
+	input [7:0] x, y,
 	output DONE,
 	output [15:0] q
 );
 
-//wires needed for ADDER
+// wires needed for ADDER
 wire [7:0] y_ADD;
 wire [7:0] z_ADD;
 wire cout;
 wire ovrflow;
 
-assign ovrflow=(x[7] ^ z_ADD[7]) & (y_ADD[7] ^ z_ADD[7]);
+assign ovrflow = (x[7] ^ z_ADD[7]) & (y_ADD[7] ^ z_ADD[7]);
 
 genvar i;
 generate
-	for(i=0;i<8;i=i+1) begin: loop
-		assign y_ADD[i]=y[i] ^ SUB;	
+	for(i=0; i<8; i=i+1) begin: loop
+		assign y_ADD[i] = y[i] ^ SUB;	
 	end
 endgenerate
 
@@ -27,9 +27,18 @@ wire ready_MULT;
 wire end_MULT;
 wire [15:0] z_MULT;
 
-//wires needed for OUT register
-wire [15:0] d_OUT;
-wire load=ADD | SUB | ready_MULT;
+// wires needed for the DIVIDER
+wire ready_DIV;
+wire [16:0] z_DIV_raw;
+wire [15:0] z_DIV = {z_DIV_raw[15:8], z_DIV_raw[7:0]}; // Rest LSB din A + Cat
+
+// wires for structural MUX cascade
+wire [15:0] z_ADD_ext = {{8{z_ADD[7] ^ ovrflow}}, z_ADD};
+wire [15:0] d_OUT_inter; 
+wire [15:0] d_OUT;       
+
+
+wire load = ADD | SUB | ready_MULT | ready_DIV;
 
 adder #(.size(8)) ADDER
 (
@@ -51,13 +60,33 @@ br4_8bits MULTIPLIER
 	.obus(z_MULT)
 );
 
-mux_1sel #(.size(16)) MUX
+nrd8bits DIVIDER
+(
+    .clk(clk),
+    .rst_b(rst_b),
+    .start(DIV),
+    .inbus({x, y}),
+    .outbus(z_DIV_raw),
+    .done(ready_DIV)
+);
+
+
+
+// MUX 1: Chooses between ADD/SUB and MULT
+mux_1sel #(.size(16)) MUX_ADD_MULT
 (
 	.sel(ready_MULT),
-
-	//we complete the 8 MSB of the 16 bit value with the propper sign of the 8 bit result 
-	.value0({{8{z_ADD[7] ^ ovrflow}},z_ADD}), 
+	.value0(z_ADD_ext),
 	.value1(z_MULT),
+	.q(d_OUT_inter)
+);
+
+// MUX 2: Chooses between previous result and DIV
+mux_1sel #(.size(16)) MUX_FINAL
+(
+	.sel(ready_DIV),
+	.value0(d_OUT_inter),
+	.value1(z_DIV),
 	.q(d_OUT)
 );
 
@@ -70,20 +99,6 @@ register #(.size(16)) OUT
 	.q(q)
 );
 
-assign DONE=end_MULT;
+assign DONE = end_MULT | ready_DIV;
 
 endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- test_benches/ALU_tb.v
+++ test_benches/ALU_tb.v
@@ -1,59 +1,66 @@
 module ALU_tb;
 
-reg clk,rst_b;
-reg ADD,SUB,MULT;
-reg [7:0] x,y;
-wire [15:0] q;
-wire DONE;
+  reg clk, rst_b;
+  reg ADD, SUB, MULT, DIV;
+  reg [7:0] x, y;
+  wire [15:0] q;
+  wire DONE;
 
-ALU test
-(
-	.clk(clk),
-	.rst_b(rst_b),
-	.ADD(ADD),
-	.SUB(SUB),
-	.MULT(MULT),
-	.x(x),
-	.y(y),
-	.DONE(DONE),
-	.q(q)
+ALU test(
+
+  .clk(clk),
+  .rst_b(rst_b),
+  .ADD(ADD),
+  .SUB(SUB),
+  .MULT(MULT),
+  .DIV(DIV),
+  .x(x),
+  .y(y),
+  .DONE(DONE),
+  .q(q)
 );
 
 initial begin
-	clk=0;
-	rst_b=1;
-	{ADD,SUB,MULT}=0;
-	forever #50 clk=~clk;
+
+clk = 0;
+
+forever #50 clk = ~clk;
+
 end
 
 initial begin
-	repeat(2) #10 rst_b=~rst_b;
-	@(negedge clk); x=77; y=-102; ADD=1;
-	@(negedge clk) ADD=0;
-	@(negedge clk); x=99; y=102; ADD=1;
-	@(negedge clk) ADD=0;
 
-	@(negedge clk); x=77; y=-102; SUB=1;
-	@(negedge clk) SUB=0;
-	@(negedge clk); x=99; y=102; SUB=1;
-	@(negedge clk) SUB=0;
+rst_b = 0;
 
-	@(negedge clk); x=77; y=-102; MULT=1;
-	@(negedge clk) MULT=0;
-	repeat(15) @(negedge clk);
-	@(negedge clk); x=99; y=102; MULT=1;
-	@(negedge clk) MULT=0;
+{ADD, SUB, MULT, DIV} = 0;
+
+x = 0; y = 0;
+
+#100 rst_b = 1;
+
 end
 
+initial begin
+  
+@(posedge rst_b);
+
+@(negedge clk);
+
+x = 77; y = 10; ADD = 1; @(negedge clk) ADD = 0;
+x = 120; y = 10; ADD = 1; @(negedge clk) ADD = 0;
+x = 50; y = 20; SUB = 1; @(negedge clk) SUB = 0;
+x = 20; y = 50; SUB = 1; @(negedge clk) SUB = 0;
+x = 12; y = -5; MULT = 1; @(negedge clk) MULT = 0;
+#500
+wait(DONE); 
+x=73; y = 21; MULT = 1; @(negedge clk) MULT = 0;
+#500
+wait(DONE);
+x = 255; y = 10; DIV = 1; @(negedge clk) DIV = 0;
+#500
+wait(DONE);
+end
+
+
+
 endmodule
-
-
-
-
-
-
-
-
-
-
-
